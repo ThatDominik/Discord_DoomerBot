16983fe6c729a8084a4a72e87c4bc3b520592f13
Dominik <dide00@vse.cz>
2023-03-17 12:49:47 +0100

    Adding more commands

--- responses.py
+++ responses.py
@@ -12,8 +12,29 @@ def handle_response(message) -> str:
         return get_link(constants.BONK)
     if message == "hentai":
         return get_link(constants.HENTAI)
+    if message == "trap":
+        return get_link(constants.TRAP)
+    if message == "uwu":
+        return get_link(constants.UWU)
+    if message == "awoo":
+        return get_link(constants.AWOO)
+    if message == "megumin":
+        return get_link(constants.MEGUMIN)
+    if message == "nom":
+        return get_link(constants.NOM)
+    if message == "blowjob":
+        return get_link(constants.BLOWJOB)
     if message == "help":
-        return "available commands:\n/doomer waifu\n/doomer bonk\n/doomer hentai\n/doomer neko"
+        return "```available commands:\n/doomer waifu\n" \
+               "/doomer bonk\n" \
+               "/doomer hentai\n" \
+               "/doomer blowjob\n" \
+               "/doomer neko\n" \
+               "/doomer uwu\n" \
+               "/doomer trap\n" \
+               "/doomer awoo\n" \
+               "/doomer megumin\n/doomer nom```"
+
     else:
         return "try /doomer help for all available commands"
 
